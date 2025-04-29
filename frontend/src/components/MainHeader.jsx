3b10218c226f9d33cbe2fa2d0fa1378b85417573
Marlisa Odokara <marlisa.od@gmail.com>
2025-04-29 14:41:04 -0500

    added link to logo

--- frontend/src/components/MainHeader.jsx
+++ frontend/src/components/MainHeader.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { useTheme } from '../context/ThemeContext'
 import classes from './MainHeader.module.css';
 import { FaSun, FaMoon } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
+
 const MainHeader = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <header className={classes.header}>
-      <h1>Mini Dashboard</h1>
+      <h1>
+        <Link to="/" className={classes.logo}>
+          Mini Dashboard
+        </Link>
+      </h1>
+      
      
       <button onClick={toggleTheme} className={classes.toggleButton}>
         {theme === 'light' ? <FaMoon /> : <FaSun />}
